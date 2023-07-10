bc7d1a0075fee3e968260e257215d4f12a807e95
jin-vita <jhj9144@gmail.com>
2023-07-10 14:40:23 +0900

    feat connect fragments

--- app/src/main/java/com/jinvita/testbottomnavigationold/MainActivity.kt
+++ app/src/main/java/com/jinvita/testbottomnavigationold/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.jinvita.testbottomnavigationold
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.jinvita.testbottomnavigationold.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() = with(binding.bottomNavView) {
+        replaceFragment(FirstFragment())
+        setOnItemSelectedListener {
+            val fragment = when (it.itemId) {
+                R.id.firstFragment -> FirstFragment()
+                R.id.secondFragment -> SecondFragment()
+                R.id.thirdFragment -> ThirdFragment()
+                R.id.fourthFragment -> FourthFragment()
+                else -> FirstFragment()
+            }
+            replaceFragment(fragment)
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) = with(supportFragmentManager) {
+        beginTransaction().replace(R.id.container, fragment).commit()
+
     }
 }
